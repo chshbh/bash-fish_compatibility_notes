5bc08b2e172fc8d736308bdb1e33882692bca484
chshbh <mailmemateat@gmail.com>
2020-09-08 09:26:58 +0530

    readme fix, xor done, functions added

--- hammingcodeinfish/README.md
+++ hammingcodeinfish/README.md
@@ -1,4 +1,4 @@
-##naive attempt at creating 1bit error correcting hamming code for 16bit string in fish.
-+ mfw there is no return value/type
+## naive attempt at creating 1bit error correcting hamming code for 16bit string in fish.
++ mfw there is no return value/type, nvm found a fix
 + mfw there is no in build decimal to binary conversion
 + mfw there is no native bitwise operator
--- hammingcodeinfish/hammingcode.fish
+++ hammingcodeinfish/hammingcode.fish
@@ -1,10 +1,29 @@
 #! /usr/bin/env fish
 
-# lesson: seldom use functions with fish
-
 function dec2bin_hax -d "this hax can map binary values upto 2^(#seq), using cross product of (0,1)*"
-    set BINARYLIST (seq 0 1)(seq 0 1)(seq 0 1)(seq 0 1)
+    set -g BINARYLIST (seq 0 1)(seq 0 1)(seq 0 1)(seq 0 1)
     echo $BINARYLIST[(math $argv+1)]
+end
+
+
+function bin2dec_hax -d "read dec2bin"
+    if contains --index $argv $BINARYLIST; ;end
+end
+
+
+function scuffed_xor -d "xor hax. how? take all the bits at significant positions and do addtion modulo 2"
+    set -g sig_bit 0 0 0 0
+    for i in $argv
+        set temp (string split '' $i)
+        set sig_bit[1] (math "$sig_bit[1]+$temp[1]")
+        set sig_bit[2] (math "$sig_bit[2]+$temp[2]")
+        set sig_bit[3] (math "$sig_bit[3]+$temp[3]")
+        set sig_bit[4] (math "$sig_bit[4]+$temp[4]")
+    end
+    set sig_bit[1] (math "$sig_bit[1]%2")
+    set sig_bit[2] (math "$sig_bit[2]%2")
+    set sig_bit[3] (math "$sig_bit[3]%2")
+    set sig_bit[4] (math "$sig_bit[4]%2")
 end
 
 # two ways to generate random string of {0,1} simulating bit string of 16bits.
@@ -16,18 +35,14 @@ for i in (seq 1 16)
     if test $bits[$i] -eq 1;set -a array (math $i-1);end
 end
 
-echo $array
+# get indices in binary 
 set binarray
 for i in $array;set -a binarray (dec2bin_hax $i);end
-echo $binarray
 
-set sig_bit 0 0 0 0
-for i in $binarray
-    set temp (string split '' $i)
-    echo $temp
-    set sig_bit[1] (math "$sig_bit[1]+$temp[1]")
-    set sig_bit[2] (math "$sig_bit[2]+$temp[2]")
-    set sig_bit[3] (math "$sig_bit[3]+$temp[3]")
-    set sig_bit[4] (math "$sig_bit[4]+$temp[4]")
-end
-echo $sig_bit
+scuffed_xor $binarray
+set sig_bit (string join "" $sig_bit)
+
+set dec (bin2dec_hax $sig_bit)
+set dec (math $dec-1)
+echo $dec
+
